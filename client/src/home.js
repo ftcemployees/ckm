aedae8b95143a161623949812f551271bf771960
jlamberti <jlamberti2015@gmail.com>
2018-05-15 17:09:44 -0600

    Fixed indentation and file name

--- client/src/home.js
+++ client/src/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Item } from './Item';
+import { Item } from './item';
 
 export const Main = () => {
     let loginInfo = '';
@@ -10,59 +10,59 @@ export const Main = () => {
     }
     return (
         <div>
-        {loginInfo}
-        <div className="home_page">
-          <header className="top_header" >
-            <h1>C K M</h1>
-            <pre className="tab">P  H  O  T  O  G  R  A  P  H  Y</pre>
-          </header>
-          <nav className="top_menu">
-            <ul>
-              <li>HOME</li>
-              <li>GALLERY</li>
-              <li>PICTURE</li>
-            </ul>
-          </nav>
-          <div className="new_div">
-            <aside className="the_aside">
-              <h4 className="words">Search</h4>
-              <input type="text" />
-              <br />
-              <br />
-              <br />
-              <p className="words">Color</p>
-              <input type="checkbox" />
-              <p className="words">Design</p>
-              <input type="checkbox" />
-              <p className="words">Author</p>
-              <input type="checkbox" />
-              <p className="words">Painting</p>
-              <input type="checkbox" />
-              <p className="words">Shape</p>
-              <input type="checkbox" />
-              <br />
-              <br />
-              <div className="filter-btn"><a>Filter</a></div>
-            </aside>
-            <section className="main_section">
-              <article>
-                  {/* Returns map of 1-15 */}
-                  {Array.apply(null, {length: 15}).map(Function.call, Number).map((r) => {
-                      return(
-                          <Item
-                              name={"pic-"+(r+1)+" pic"}
-                              desc="description"
-                          />
-                      )
-                  })}
-              </article>
-            </section>
-          </div>
-          <footer className="the_footer">
-            © Copyright Lu Yuan Ting 2018
-          </footer>
+            {loginInfo}
+            <div className="home_page">
+                <header className="top_header" >
+                    <h1>C K M</h1>
+                    <pre className="tab">P  H  O  T  O  G  R  A  P  H  Y</pre>
+                </header>
+                <nav className="top_menu">
+                    <ul>
+                        <li>HOME</li>
+                        <li>GALLERY</li>
+                        <li>PICTURE</li>
+                    </ul>
+                </nav>
+                <div className="new_div">
+                    <aside className="the_aside">
+                        <h4 className="words">Search</h4>
+                        <input type="text" />
+                        <br />
+                        <br />
+                        <br />
+                        <p className="words">Color</p>
+                        <input type="checkbox" />
+                        <p className="words">Design</p>
+                        <input type="checkbox" />
+                        <p className="words">Author</p>
+                        <input type="checkbox" />
+                        <p className="words">Painting</p>
+                        <input type="checkbox" />
+                        <p className="words">Shape</p>
+                        <input type="checkbox" />
+                        <br />
+                        <br />
+                        <div className="filter-btn"><a>Filter</a></div>
+                    </aside>
+                    <section className="main_section">
+                        <article>
+                            {/* Returns map of 1-15 */}
+                            {Array.apply(null, {length: 15}).map(Function.call, Number).map((r) => {
+                                return(
+                                    <Item
+                                        name={"pic-"+(r+1)+" pic"}
+                                        desc="description"
+                                    />
+                                )
+                            })}
+                        </article>
+                    </section>
+                </div>
+                <footer className="the_footer">
+                    © Copyright Lu Yuan Ting 2018
+                </footer>
+            </div>
         </div>
-        </div>
-	
+
     )
 }
