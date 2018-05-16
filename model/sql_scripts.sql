f1ba224e641a0f17cae516dc4714ffe365655af3
jlamberti <jlamberti2015@gmail.com>
2018-05-16 13:04:46 -0600

    Current changes

--- model/sql_scripts.sql
+++ model/sql_scripts.sql
@@ -1,13 +1,39 @@
 create table users (
-  id int not null auto_increment,
-  user_name text not null,
-  pwd text not null,
-  email text not null,
-  name_first text not null,
-  name_last text not null,
-  created_at datetime default now(),
-  updated_at datetime default now(),
-  deleted_at datetime,
-  
-  constraint pk_users primary key(id)
-);
+    id int not null auto_increment,
+    user_name text not null,
+    pwd text not null,
+    email text not null,
+    name_first text not null,
+    name_last text not null,
+    created_by int not null,
+    creation_date datetime default sysdate(),
+
+    constraint pk_users primary key(id)
+  );
+
+--create table item (
+--    id int not null auto_increment,
+--    img_path varchar2(255),
+--    description	varchar2(255),
+--    name varchar2(255),
+--    created_by int not null,
+--    creation_date datetime default sysdate(),
+--
+--    constraint pk_users primary key(id)
+--  );
+--
+--create table tag (
+--    id int not null auto_increment,
+--    name varchar2(255),
+--    created_by int not null,
+--    creation_date datetime default sysdate(),
+--
+--    constraint pk_users primary key(id)
+--  );
+--
+--create table item_tag (
+--    tag_id	int,
+--    item_id	int,
+--    created_by int not null,
+--    creation_date datetime default sysdate(),
+--  );
