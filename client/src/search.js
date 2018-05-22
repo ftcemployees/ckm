50fc541ebd9f9f63b04cdecbde7b7e9789f5b7ce
jlamberti <jlamberti2015@gmail.com>
2018-05-22 17:58:01 -0600

    Removed unnecessary console logs and added default tags

--- client/src/search.js
+++ client/src/search.js
@@ -2,64 +2,7 @@ import React from 'react';
 import Autosuggest from 'react-autosuggest';
 import Axios from 'axios';
 
-let tags = [
-    {
-        name: 'C',
-        year: 1972
-    }
-    // {
-    //     name: 'C#',
-    //     year: 2000
-    // },
-    // {
-    //     name: 'C++',
-    //     year: 1983
-    // },
-    // {
-    //     name: 'Clojure',
-    //     year: 2007
-    // },
-    // {
-    //     name: 'Elm',
-    //     year: 2012
-    // },
-    // {
-    //     name: 'Go',
-    //     year: 2009
-    // },
-    // {
-    //     name: 'Haskell',
-    //     year: 1990
-    // },
-    // {
-    //     name: 'Java',
-    //     year: 1995
-    // },
-    // {
-    //     name: 'Javascript',
-    //     year: 1995
-    // },
-    // {
-    //     name: 'Perl',
-    //     year: 1987
-    // },
-    // {
-    //     name: 'PHP',
-    //     year: 1995
-    // },
-    // {
-    //     name: 'Python',
-    //     year: 1991
-    // },
-    // {
-    //     name: 'Ruby',
-    //     year: 1995
-    // },
-    // {
-    //     name: 'Scala',
-    //     year: 2003
-    // }
-];
+let tags = [];
 
 // https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions#Using_Special_Characters
 function escapeRegexCharacters(str) {
@@ -105,20 +48,12 @@ export class SearchComponent extends React.Component {
     }
 
     async loadTags() {
-        console.log("Egg");
         Axios.get('/tag_suggestion')
             .then(function (response) {
-                // console.log(response);
                 tags = {data: response.data};
-                // tags = response.data.map((item, index) => {
-                //     console.log(index + ' || ' + item);
-                //     tags += item;
-                // })
-                console.log(tags);
             })
             .catch(function (error) {
-                console.log("here2");
-                const dataTemp = [{name: 'beaded'},{name: 'bell'},{name: 'black'},{name: 'boiled'}];
+                const dataTemp = {data: [{name: 'beaded'},{name: 'bell'},{name: 'black'},{name: 'boiled'}]};
                 tags = [{data: dataTemp}];
                 console.log(error);
             });
