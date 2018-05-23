696ae67c8e0d7ec26c897113292057968bc55aaf
jlamberti <jlamberti2015@gmail.com>
2018-05-23 17:26:48 -0600

    Fixed gallery error handling

--- client/src/gallery.js
+++ client/src/gallery.js
@@ -40,10 +40,9 @@ export class Gallery extends React.Component {
                 self.setState({data: [{id: 1},{id: 2},{id: 3},{id: 4},{id: 5},{id: 6},{id: 7},{id: 8},{id: 9},{id: 10},{id: 11},{id: 12},{id: 13},{id: 14},{id: 15}]})
         })
         .catch(function (error) {
-            // const dataTemp = [{id: 1},{id: 2},{id: 3},{id: 4},{id: 5},{id: 6},{id: 7},{id: 8},{id: 9},{id: 10},{id: 11},{id: 12},{id: 13},{id: 14},{id: 15}];
-            // self.setState({data: dataTemp});
-            // //console.log(dataTemp);
-            // console.log(error);
+            const dataTemp = [{id: 1},{id: 2},{id: 3},{id: 4},{id: 5},{id: 6},{id: 7},{id: 8},{id: 9},{id: 10},{id: 11},{id: 12},{id: 13},{id: 14},{id: 15}];
+            self.setState({data: dataTemp});
+            console.log(error);
         });
     }
 
