2ec1dc06299a45954bd18e4c59d9515323f897d5
jlamberti <jlamberti2015@gmail.com>
2018-05-29 18:05:15 -0600

    Fixed header, added logo

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Ckm } from './app-ckm';
 import './css/main.css';
+import WebFont from 'webfontloader';
 
 ReactDOM.render(<Ckm />, document.getElementById('root'));
+
+WebFont.load({
+    google: {
+        families: ['Titillium Web:300,400,700', 'sans-serif']
+    }
+});
