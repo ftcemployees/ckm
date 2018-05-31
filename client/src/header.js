3178eebdccd98b0352b54c21ff97815362e88dd5
jlamberti <jlamberti2015@gmail.com>
2018-05-31 11:45:18 -0600

    Removed unnecessary console logs and added default tags

--- client/src/header.js
+++ client/src/header.js
@@ -11,16 +11,19 @@ export class Header extends React.Component {
         }
 
         return (
-            <div>
-                <div className="header">
+            <div className="global-nav-container">
+                <div className="ckm-logo-container">
+                    CKM
+                </div>
+                <div className="global-nav-tools">
                     {/*<input type="text" placeholder="Search.."/>*/}
                     <SearchComponent/>
                     {loginInfo}
                 </div>
-                <header className="top_header" >
-                    <h1>C K M</h1>
-                    <pre className="tab">P  H  O  T  O  G  R  A  P  H  Y</pre>
-                </header>
+                {/*<header className="top_header" >*/}
+                    {/*<h1>C K M</h1>*/}
+                    {/*<pre className="tab">P  H  O  T  O  G  R  A  P  H  Y</pre>*/}
+                {/*</header>*/}
             </div>
         );
     }
